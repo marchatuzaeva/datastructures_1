6fd33d2a2c2811d887b0d28d18dc73750323adf8
marchatuzaeva <marcha.tuzaeva@edu.fh-joanneum.at>
2025-05-06 17:38:23 +0200

    Update Single Linked List

--- datastructures_1/src/main/java/at/fhj/msd/MySinglyLinkedList.java
+++ datastructures_1/src/main/java/at/fhj/msd/MySinglyLinkedList.java
@@ -1,20 +1,53 @@
 package at.fhj.msd;
 
-public class MySinglyLinkedList {
-  Node head;
+public class MySinglyLinkedList<E> {
+  private static class Node<E> {
+    E data;
+    Node<E> next;
 
-  public MySinglyLinkedList(Node head) {
-    this.head = head;
+    public Node(E data, Node<E> next) {
+      this.data = data;
+      this.next = next;
+
+    }
   }
-}
 
-class Node {
-  int data;
-  Node next;
+  private Node<E> head;
+  private int size = 0;
 
-  public Node(int data) {
-    this.data = data;
-    this.next = null;
+  public MySinglyLinkedList() {
+    head = null;
+    size = 0;
+  }
 
+  public int size() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    if (size == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public void addFirst(E element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Element cannot be null");
+    }
+
+    Node<E> newNode = new Node<>(element, head);
+    head = newNode;
+    size++;
+  }
+
+  public E removeFirst() {
+    if (isEmpty())
+      return null;
+    E temp = head.data;
+    head = head.next;
+    size--;
+    return temp;
   }
 }
