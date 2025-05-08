da5d511a5a9feadc6f6a6b7e4a92bcf91283e88a
Linda Kadyrova <linda.kadyrova@edu.fh-joanneum.at>
2025-05-08 11:22:33 +0200

    Fix calculator >postfix

--- datastructures_1/src/main/java/at/fhj/msd/MyCalculator.java
+++ datastructures_1/src/main/java/at/fhj/msd/MyCalculator.java
@@ -1,41 +1,58 @@
 package at.fhj.msd;
 
-import java.util.Stack;
-
 public class MyCalculator {
 
-  public static int evaluate(String expression) {
-    Stack<Integer> stack = new Stack<>();
+    public static int evaluatePostfix(String expression) {
+        MySinglyLinkedList<Integer> stack = new MySinglyLinkedList<>();
 
-    for (String token : expression.split(" ")) {
-      switch (token) {
-        case "+":
-          stack.push(stack.pop() + stack.pop());
-          break;
-        case "-":
-          int b = stack.pop();
-          int a = stack.pop();
-          stack.push(a - b);
-          break;
-        case "*":
-          stack.push(stack.pop() * stack.pop());
-          break;
-        case "/":
-          int divisor = stack.pop();
-          int dividend = stack.pop();
-          stack.push(dividend / divisor);
-          break;
-        default:
-          stack.push(Integer.parseInt(token));
-      }
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+
+            // Ignoriere Leerzeichen
+            if (c == ' ') {
+                continue;
+            }
+
+            // Falls die Zahl ist, füge sie in den Stack ein
+            if (Character.isDigit(c)) {
+                StringBuilder num = new StringBuilder();
+                while (i < expression.length() && Character.isDigit(expression.charAt(i))) {
+                    num.append(expression.charAt(i));
+                    i++;
+                }
+                i--;  // Um sicherzustellen, dass die Schleife den richtigen Index beibehält
+                stack.addFirst(Integer.parseInt(num.toString()));
+            }
+            // Wenn der Operator ist, führe die Operation aus
+            else {
+                int b = stack.removeFirst();
+                int a = stack.removeFirst();
+
+                switch (c) {
+                    case '+':
+                        stack.addFirst(a + b);
+                        break;
+                    case '-':
+                        stack.addFirst(a - b);
+                        break;
+                    case '*':
+                        stack.addFirst(a * b);
+                        break;
+                    case '/':
+                        stack.addFirst(a / b);
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Unbekannter Operator: " + c);
+                }
+            }
+        }
+
+        // Der letzte Wert im Stack ist das Ergebnis
+        return stack.removeFirst();
     }
 
-    return stack.pop();
-  }
-
-  public static void main(String[] args) {
-    String expression = "5 1 2 + 4 * + 3 -";  
-    int result = evaluate(expression);
-    System.out.println("Result: " + result);
-  }
+    public static void main(String[] args) {
+        String expression = "3 4 + 2 * 7 /";
+        System.out.println("Ergebnis: " + evaluatePostfix(expression));  // Sollte 2 ausgeben
+    }
 }
